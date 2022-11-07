102b012b39d8af42fb0cc181e901172d57fbccfa
Gabriel Alves <114401117+Gabzin14@users.noreply.github.com>
2022-11-07 19:16:19 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function retornaValores(conteudo) {
+    if(!("erro" in conteudo)) {
+   document.getElementById('rua').value=(conteudo.logradouro)
+ document.getElementById('bairro').value=(conteudo.bairro)
+ document.getElementById('localidade').value=(conteudo.localidade) }else{
+    alert("CEP não encontrado")
+    }
+}
+
+function buscaCep(valor) {
+    var cep = valor.replace(/\D/g, '')
+    if(cep != "") {
+        var validarCEP = /^[0-9]{8}$/
+        if(validarCEP.test(cep)) {
+            document.getElementById ('rua').value = "..."
+            document.getElementById ('bairro').value = "..."
+            document.getElementById ('localidade').value = "..."
+            var script = document.createElement ('script')
+            script.src = 'https://viacep.com.br/ws/${cep}/json/?callback=retornavalores'
+            document.body.appendChild(script)
+          }else{
+              alert("Formato CEP Inválido")
+          }
+    }
+}
